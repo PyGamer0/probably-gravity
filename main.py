8b5393eb0455c2a2996594113037ad768ebd8722
pygamer0 <maanasyt@gmail.com>
2021-05-17 14:14:36 +0530

    Better sim

--- main.py
+++ main.py
@@ -1,58 +1,80 @@
-from freegames import vector
-import pyglet as p
+import numpy as np    # Numpy for vectors
+import pyglet
 
-class entity:
-    def __init__(self, m, x, y):
-        self.pos = vector(x, y)
-        self.vel = vector(0, 0)
-        self.acc = vector(0, 0)
-        self.mass = m
+# Class 
 
-    def apply_force(self, f):
-        self.acc += f / self.mass
+class body:
+    def __init__(self, x, y, mass, vx=0, vy=0):
+        self.position = np.array([x, y])
+        self.velocity = np.array([vx, vy])
+        self.acceleration = np.array([0, 0])
+        self.mass = mass
+        self.r = 25 * (self.mass / 2)
 
-    def apply_gravity(self, other):
-        # Calculate distances
-        dx = other.pos.x - self.pos.x
-        dy = other.pos.y - self.pos.y
-
-        # Force
-        mg = dx ** 2 + dy ** 2
-        f = 1/1000 * other.mass * vector(mg / dx, mg / dy)
-
-        self.apply_force(f)
-
-
-    def update(self):
-        self.vel += self.acc
-        self.pos += self.vel
-        self.acc *= 0
-
-    def draw(self, c, r):
-        shape = p.shapes.Circle(self.pos.x, self.pos.y, r)
-        shape.color = c
+    def draw(self, color):
+        shape = pyglet.shapes.Circle(self.position[0], self.position[1], self.r)
+        shape.color = color
         shape.draw()
 
-win = p.window.Window(width=500, height=500)
-e1 = entity(1, 200, 200)
-e2 = entity(1, 300, 300)
-e1.apply_force(vector(0.01, 0.0000751))
-# e2.apply_force(vector(-0.01, -0.0000751))
+    def update(self):
+        VELOCITY_CAP = 10
+        if self.velocity[0] > VELOCITY_CAP:
+            self.velocity[0] = VELOCITY_CAP
+        if self.velocity[1] > VELOCITY_CAP:
+            self.velocity[1] = VELOCITY_CAP
 
-def update(t):
-    e1.update()
-    e2.update()
+        if self.velocity[0] < -VELOCITY_CAP:
+            self.velocity[0] = -VELOCITY_CAP
+        if self.velocity[1] < -VELOCITY_CAP:
+            self.velocity[1] = -VELOCITY_CAP
 
-    e1.apply_gravity(e2)
+        self.velocity = self.velocity + self.acceleration
+        self.position = self.position + self.velocity
+        self.acceleration *= 0
 
-    e2.apply_gravity(e1)
+    def apply_force(self, force):
+        # self.acceleration += force / self.mass
+        self.acceleration = self.acceleration + (force / self.mass)
+
+    def gravity(self, other):
+        distv = other.position - self.position
+        dist = np.linalg.norm(distv)
+        normal = distv / dist
+
+        G = 1
+        force = G * self.mass * other.mass * 1/dist * normal
+        
+        self.apply_force(force)
+
+
+win = pyglet.window.Window(500, 500, "Gravity")
+
+# Make bodies
+# TODO: Make this expandible using a list
+b1 = body(200, 200, 1, 0.1, 1)
+b2 = body(300, 300, 1, -0.2, -1)
+# b3 = body(200, 300, 1, 0.1, 0.1)
+
+def update(dt):
+    b1.gravity(b2)
+    b2.gravity(b1)
+    # b1.gravity(b3)
+    # b2.gravity(b3)
+    # b3.gravity(b1)
+    # b3.gravity(b2)
+
+    b1.update()
+    b2.update()
+    # b3.update()
 
 @win.event
 def on_draw():
     win.clear()
-    e1.draw((217, 32, 134), 25)
-    e2.draw((31, 103, 231), 25)
+    b1.draw((245, 24, 134))
+    b2.draw((145, 20, 233))
+    # b3.draw((34, 234, 123))
 
-p.clock.schedule_interval(update, 1/256)
-p.app.run()
+
+pyglet.clock.schedule_interval(update, 1/999999)     # 60 frames per second (60 fps)
+pyglet.app.run()
 
